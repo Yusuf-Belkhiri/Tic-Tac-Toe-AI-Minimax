cf5d3294898b788956e28b8a5cb86137efcae735
Yusuf-Belkhiri <youcefbelkhiri005@gmail.com>
2023-05-01 07:51:54 +0100

    Win detection

--- script.js
+++ script.js
@@ -12,11 +12,9 @@ const winCombos = [
     [6, 4, 2]
 ]
 
-
 const cells = document.querySelectorAll('.cell')
 
 startGame();
-
 
 function startGame(){
     //document.querySelector(".endgame").style.display = "None"
@@ -26,11 +24,11 @@ function startGame(){
     for (var i = 0; i < cells.length; i++){
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
-        cells[i].addEventListener('click', onCellClick, false);
+        cells[i].addEventListener('click', onClickCell, false);
     }
 }
 
-function onCellClick(cell){
+function onClickCell(cell){
     selectCell(cell.target.id, humanPlayer);
 }
 
@@ -38,4 +36,36 @@ function selectCell(cellId, player){
     origBoard[cellId] = player;
     cells[cellId].innerText = player;
     //document.getElementById(cellId).innerText = player;
+
+    let gameWon = checkWin(origBoard, player);
+    if(gameWon)
+        gameOver(gameWon);
+}
+
+function checkWin(board, player){
+    // Get all the current player's plays
+    let plays = board.reduce((a, e, i) =>       // Array.reduce(callbackfct) a: accumulator(return value), it accumulates the callback's return values
+     (e === player) ? a.concat(i) : a, []);        // e: element, i: index, []: initial value of a 
+
+    // Check if any winCombo is achieved
+    let gameWon = null;
+    for(let [winComboIndex, winCombo] of winCombos.entries()){
+        if(winCombo.every(elem => plays.indexOf(elem) > -1)){
+            gameWon = {winComboIndex: winComboIndex, player: player};
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon){
+    // Colorize winCombo cells
+    for(let cellIndex of winCombos[gameWon.winComboIndex]){
+        document.getElementById(cellIndex).style.backgroundColor = gameWon.player == humanPlayer ? "blue" : "red";
+    }
+
+    // Disable click event
+    for(let cell of cells){
+        cell.removeEventListener('click', onClickCell, false);
+    }
 }
